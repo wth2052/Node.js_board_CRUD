09a38d57ed35e79ab0e24475a64eac8fefed6218
“Woo <wth2052@gmail.com>
2022-12-15 20:32:20 +0900

    오예

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-// node_modules의 express 패키지를 가져온다.
-var express = require('express')
-const session = require('express-session')
-const bodyParser = require('body-parser');
-const FileStore = require('session-file-store')(session)
+//router 가져오기
+const routers = require("./routes")
+const connect = require("./schemas/index.js");
+connect();
+const cors = require("cors")
+// CORS(Cross-Origin Resource Sharing)란
+// 자신이 속하지 않은 다른 도메인, 다른 프로토콜, 혹은 다른 포트에 있는 리소스를 요청하는 cross-origin HTTP 요청 방식
+// https://surprisecomputer.tistory.com/32
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(cors());
+app.use(express.static("static"))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json());
 
 
+// app.use(requestMiddleware);
+app.use("/api", routers)
 
-
-
-// express 서버를 실행할 때 필요한 포트 정의 및 실행 시 callback 함수를 받습니다
-//환경변수에서 port를 가져온다. 환경변수가 없을시 3000포트를 지정한다.
-const portnumber = Number(3000)
-var port = app.listen(process.env.PORT || portnumber);
 
 app.listen(port, () => {
-    console.log(`Express, server start at ${portnumber}`);
-})
+  console.log('***************** ***************** *****************')
+  console.log('********** 서버가 정상적으로 실행되고 있습니다 *********')
+  console.log('*************** 현재 포트 : '+port+'***************')
+  console.log('***************** ***************** *****************')
+});
--- routes/comments copy.js
+++ routes/comments copy.js
@@ -2,6 +2,7 @@
 const express = require("express")
 const router = express.Router()
 const Posts = require("../schemas/posts")
+
 //GET 댓글 조회 완성
 router.get("/:postsId", async (req, res) => {
   const { postsId } = req.params;
@@ -9,29 +10,27 @@ router.get("/:postsId", async (req, res) => {
   res.json({ getAllComments })
 })
 
-//POST 댓글 작성 진행중
 router.post("/:postsId", async (req, res) => {
-  
   const { postsId } = req.params;
   const {nickname, content} = req.body
-  const post = await Posts.findById (postsId)
+  const post = await Posts.findOne ({ postsId : postsId})
   const date = new Date()
   const commentid = date.valueOf();
-  console.log(post)
-  res.json(post)
-  try {
-        post.comments.create({
-        nickname, content, commentid
+
+    try {
+        post.comments.push({
+        commentid: commentid,
+        nickname: nickname,
+        content: content,
+        createdAt: date,
     });
       await post.save();
       return res.status(200).send({message: "댓글 작성 성공!"});
-    } catch (err){
-      console.log(err) 
+    } catch (err){ 
       if(!content || !nickname){
         return res.status(400).json({success: false, errorMessage:"내용이나 작성자가 입력되지 않았습니다. 다시한번 확인해주세요."});
       }
     }
-
 });
 
 module.exports = router;
